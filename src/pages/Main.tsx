4cb67f9053d3b24cd9e83c5248583803a6d12f4d
Dmitiy Korn <cloackz@yandex.ru>
2024-10-01 10:22:17 +0400

    Update main

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,22 +1,135 @@
-import { Container } from "@/components/ui";
 import Layout from "@/components/layout/Layout";
 import { Helmet } from "react-helmet-async";
-import { useTelegram } from "@/hooks/useTelegram";
-import { MainContent } from "@/components/MainContent";
-import { SButtonClose, SPageContent } from "@/pages/styled";
+import { styled } from "@mui/material";
+import { Container } from "@/components/ui";
+import { useEffect, useMemo, useRef, useState } from "react";
+
+import VolumeUpOutlinedIcon from "@mui/icons-material/VolumeUpOutlined";
+import VolumeOffOutlinedIcon from "@mui/icons-material/VolumeOffOutlined";
+
+const quotes = [
+  "Прод — это когда все работает, но" + " никто не знает, почему.",
+  "Дедлайны — это просто рекомендованные даты.",
+  "Чем больше" + " костылей, тем ближе к продакшену.",
+  "Рефакторинг — это когда ты переделываешь старые баги в новые.",
+  "Код должен быть понятен только тому, кто его писал.",
+];
+
+const Block = styled("div")`
+  padding: 40px 0 120px 0;
+`;
+
+const VideoWrapper = styled("div")`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  video {
+    margin-left: 40px;
+    object-fit: cover;
+    height: 300px;
+  }
+`;
+
+const List = styled("ul")`
+  display: flex;
+  flex-direction: column;
+  gap: 60px;
+`;
+
+const Item = styled("ul")`
+  quotes: "«" "»" "‹" "›";
+  font-size: 32px;
+`;
+
+const MuteButton = styled("button")`
+  cursor: pointer;
+  position: fixed;
+  top: 20px;
+  right: 20px;
+  border: none;
+  background: transparent;
+  color: #fff;
+  border: 1px solid #fff;
+  border-radius: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 15px;
+`;
 
 const Main = () => {
+  const [mute, setMute] = useState(false);
+  const [showNavBar, setShowNavBar] = useState(false);
+  const audioRef = useRef(null);
+  const blockRef = useRef(null);
+
+  // Мемоизация видео
+  const videoElement = useMemo(
+    () => (
+      <video autoPlay loop={true} controls={false}>
+        <source src={"/alien.mp4"} />
+      </video>
+    ),
+    [],
+  );
+
+  // Мемоизация аудио
+  const audioElement = useMemo(
+    () => (
+      <audio ref={audioRef} autoPlay loop muted={mute}>
+        <source src={"/sound.mp3"} type='audio/mp3' />
+      </audio>
+    ),
+    [mute],
+  );
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const block = blockRef.current;
+      if (block) {
+        const blockBottom = block.getBoundingClientRect().bottom;
+        const windowHeight = window.innerHeight;
+
+        setShowNavBar(blockBottom < windowHeight + 50);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleMuteClick = () => {
+    setMute(prevMute => !prevMute);
+    if (audioRef.current) {
+      audioRef.current.muted = !audioRef.current.muted;
+    }
+  };
+
   return (
     <>
       <Helmet>
         <title>Ретро 2024</title>
       </Helmet>
-      <Layout>
-        <SPageContent sx={{ paddingTop: "100px", position: "relative" }}>
+      <Layout showNavBar={showNavBar}>
+        <Block id='block' ref={blockRef}>
+          <MuteButton onClick={handleMuteClick}>
+            {mute ? <VolumeOffOutlinedIcon /> : <VolumeUpOutlinedIcon />}
+          </MuteButton>
           <Container>
-            <MainContent />
+            <VideoWrapper>{videoElement}</VideoWrapper>
+            {audioElement}
+            <List>
+              {quotes.map((item, index) => (
+                <Item key={index}>
+                  <q>{item}</q>
+                </Item>
+              ))}
+            </List>
           </Container>
-        </SPageContent>
+        </Block>
       </Layout>
     </>
   );
