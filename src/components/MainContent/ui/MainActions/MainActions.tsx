05c08e7fdf3c1a82c67ca28af9f8f1380239244b
Cloackz <Cloackz@yandex.ru>
2024-12-05 16:32:57 +0400

    Get user info

--- src/components/MainContent/ui/MainActions/MainActions.tsx
+++ src/components/MainContent/ui/MainActions/MainActions.tsx
@@ -1,58 +1,5 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import { Box, Button } from "@mui/material";
-import { SBlockWrapper, SActionButton, SButtonClose } from "./styled";
-import catSisadmin from "@/assets/img/cat-sisadmin2.jpg";
-
-import { useTelegram } from "@/hooks/useTelegram";
-
 type MainActionsProps = {
   sectionId: string;
 };
 
-export const MainActions = ({ sectionId }: MainActionsProps) => {
-  const { tg, user, onClose } = useTelegram();
-  const [sended, setSended] = useState(false);
-
-  const onSendData = useCallback(async () => {
-    setSended(true);
-    const data = {
-      username: `${user}`,
-      user: user,
-    };
-
-    await tg.sendData(JSON.stringify(data));
-  }, [user?.username]);
-
-  useEffect(() => {
-    tg.onEvent("mainButtonClicked", onSendData);
-    return () => {
-      tg.offEvent("mainButtonClicked", onSendData);
-    };
-  }, [onSendData]);
-
-  useEffect(() => {
-    tg.MainButton.setParams({
-      text: "Отправить данные",
-    });
-  }, []);
-
-  return (
-    <section id={sectionId} style={{ marginTop: "60px" }}>
-      <Box>
-        <SBlockWrapper>
-          <SActionButton onClick={onSendData}>
-            Передать нам привет
-          </SActionButton>
-          {sended && (
-            <Box sx={{ mt: 4, color: "#fff" }}>
-              Привет, {`${user?.username}`}
-              {/*<Box sx={{ mt: 2, mb: 3 }}>*/}
-              {/*  <img src={catSisadmin} alt='Привет' />*/}
-              {/*</Box>*/}
-            </Box>
-          )}
-        </SBlockWrapper>
-      </Box>
-    </section>
-  );
-};
+export const MainActions = ({ sectionId }: MainActionsProps) => {};
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,162 +1,31 @@
+//@ts-nocheck
 import Layout from "@/components/layout/Layout";
+import { useTelegram } from "@/hooks/useTelegram";
 import { Helmet } from "react-helmet-async";
-import { Container } from "@/components/ui";
-import { useEffect, useMemo, useRef, useState } from "react";
-import {
-  Block,
-  Hero,
-  ArrowIcon,
-  VideoWrapper,
-  QuotesBlock,
-  List,
-  Item,
-  MuteButton,
-  WelcomeBlock,
-} from "./styled";
-
-import VolumeUpOutlinedIcon from "@mui/icons-material/VolumeUpOutlined";
-import VolumeOffOutlinedIcon from "@mui/icons-material/VolumeOffOutlined";
-
-import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
-
-import alien from "@/assets/video/alien.gif";
-import sound from "@/assets/sound/sound.mp3";
-import { Button } from "@mui/material";
-
-const quotes = [
-  "Прод — это когда все работает, но" + " никто не знает, почему.",
-  "Дедлайны — это просто рекомендованные даты.",
-  "Чем больше" + " костылей, тем ближе к продакшену.",
-  "Рефакторинг — это когда ты переделываешь старые баги в новые.",
-  "Код должен быть понятен только тому, кто его писал.",
-];
 
 const Main = () => {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [showNavBar, setShowNavBar] = useState(false);
-  const audioRef = useRef<HTMLAudioElement | null>(null);
-  const blockRef = useRef<HTMLDivElement | null>(null);
-  const [start, setStart] = useState(true);
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
-  useEffect(() => {
-    if (start) {
-      document.body.style.overflow = "hidden";
-    } else {
-      document.body.style.overflow = "auto";
-    }
-
-    return () => {
-      document.body.style.overflow = "auto";
-    };
-  }, [start]);
-
-  useEffect(() => {
-    const audio = audioRef.current;
-
-    if (audio) {
-      audio.volume = 0.5;
-    }
-
-    const handleScroll = () => {
-      const block = blockRef.current;
-      if (block) {
-        const blockBottom = block.getBoundingClientRect().bottom;
-        const windowHeight = window.innerHeight;
-        setShowNavBar(blockBottom < windowHeight + 50);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  const handleAudioToggle = () => {
-    const audio = audioRef.current;
-
-    if (isPlaying) {
-      audio?.pause();
-    } else {
-      audio?.play().catch((error: unknown) => {
-        console.error("Error playing audio:", error);
-      });
-    }
-    setIsPlaying(prevIsPlaying => !prevIsPlaying);
-  };
-
-  const handleInitialPlay = () => {
-    const audio = audioRef.current;
-
-    if (audio) {
-      audio
-        .play()
-        .then(() => {
-          setIsPlaying(true);
-        })
-        .catch((error: unknown) => {
-          console.error("Error playing audio on initial click:", error);
-        });
-    }
-  };
-
-  const handleStart = () => {
-    setStart(false);
-    handleInitialPlay();
-  };
+  const { user, chatId } = useTelegram();
 
   return (
     <>
       <Helmet>
         <title>Ретро 2024</title>
       </Helmet>
-      <Layout showNavBar={!start && showNavBar}>
-        {start && (
-          <WelcomeBlock>
-            <Button onClick={handleStart}>Начать</Button>
-          </WelcomeBlock>
-        )}
-        <Block id='block' ref={blockRef}>
-          <MuteButton
-            onClick={isPlaying ? handleAudioToggle : handleInitialPlay}
-          >
-            {!isPlaying ? <VolumeOffOutlinedIcon /> : <VolumeUpOutlinedIcon />}
-          </MuteButton>
-          <Hero>
-            <Container>
-              <h1>
-                DEV UNIT -<br /> спешит на помощь
-              </h1>
-              <VideoWrapper>
-                <img src={alien} alt={"флекс"} />
-              </VideoWrapper>
-
-              <ArrowIcon>
-                <ArrowDownwardIcon />
-              </ArrowIcon>
-            </Container>
-          </Hero>
-          <Container>
-            <QuotesBlock>
-              <h2> Цитаты великих разработчиков:</h2>
-              <List>
-                {quotes.map((item, index) => (
-                  <Item key={index}>
-                    <q>{item}</q>
-                  </Item>
-                ))}
-              </List>
-            </QuotesBlock>
-          </Container>
-
-          <audio ref={audioRef} loop>
-            <source src={sound} type='audio/mp3' />
-          </audio>
-        </Block>
+      <Layout>
+        <div>
+          <h1>Информация о пользователе:</h1>
+          {user ? (
+            <div>
+              <p>Имя: {user.first_name}</p>
+              <p>Фамилия: {user.last_name || "Не указана"}</p>
+              <p>Username: {user.username || "Не указан"}</p>
+              <p>ID: {user.id}</p>
+            </div>
+          ) : (
+            <p>Пользователь не найден</p>
+          )}
+          <h2>Chat ID: {chatId || "Недоступен"}</h2>
+        </div>
       </Layout>
     </>
   );
